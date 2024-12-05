717e406fcf004e7acb07b5807e0436566d08b345
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-12-05 14:49:56 -0500

    servo code

--- src/temp_code/servo_motor.py
+++ src/temp_code/servo_motor.py
@@ -2,5 +2,18 @@
 import RPi.GPIO as GPIO
 import time 
 
-# Board setup 
-print("nominal")
+# Board setup
+servoGpioPin = 18
+GPIO.setmode(GPIO.BCM) # BCM: Broadcom SOC channel
+GPIO.setup(servoGpioPin, GPIO.OUT)
+
+pin = GPIO.PWM(servoGpioPin, 50) # Set GPIO pin 17 to operate at 50Hz 
+pin.start(0) # Duty cycle for MG996R servo - units ms
+try:
+    while True:
+        pin.ChangeDutyCycle(7.5) # 0
+        time.sleep(2)
+        
+except KeyboardInterrupt:
+    pin.stop()
+    GPIO.cleanup()
