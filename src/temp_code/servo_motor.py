2f7d617b572b2fda5ae2f0551c02be6ee32bdf28
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-12-05 15:05:29 -0500

    gpiozero code

--- src/temp_code/servo_motor.py
+++ src/temp_code/servo_motor.py
@@ -1,39 +1,17 @@
-import gpiod
+# Import RPi libraries
+import RPi.GPIO as GPIO # For PWM
 import time
+from gpiozero import AngularServo # For direct commands to servo
 
-# Constants
-CHIP = "gpiochip0"  # Default GPIO chip
-LINE_OFFSET = 18    # BCM pin 18
-PWM_FREQUENCY = 50  # Frequency in Hz (standard for servo motors)
-DUTY_NEUTRAL = 7.5  # Neutral position duty cycle (in %)
+servo = AngularServo(18, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
-# Calculate the period and pulse width
-PERIOD_NS = int(1_000_000_000 / PWM_FREQUENCY)  # Period in nanoseconds
-DUTY_NS = int((DUTY_NEUTRAL / 100) * PERIOD_NS) # Neutral position in nanoseconds
-
-# Setup GPIO
-chip = gpiod.Chip(CHIP)
-line = chip.get_line(LINE_OFFSET)
-
-config = gpiod.LineRequest()
-config.consumer = "servo_control"
-config.request_type = gpiod.LINE_REQUEST_DIRECTION_OUTPUT
-
-line.request(config)
+def set_angle(angle):
+    servo.angle = angle
+    time.sleep(1)
 
 try:
-    # Generate PWM manually
     while True:
-        # High phase
-        line.set_value(1)
-        time.sleep(DUTY_NS / 1_000_000_000)  # Convert ns to seconds
-        
-        # Low phase
-        line.set_value(0)
-        time.sleep((PERIOD_NS - DUTY_NS) / 1_000_000_000)
+        set_angle(60)
 
 except KeyboardInterrupt:
-    # Cleanup
-    line.set_value(0)
-    line.release()
-    chip.close()
+    print("Program exiting...")
