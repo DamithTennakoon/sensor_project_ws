b0bebd49bfe8396a8c56d9fab22a3adee65fbfe3
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-12-05 15:25:00 -0500

    code

--- src/temp_code/servo_motor.py
+++ src/temp_code/servo_motor.py
@@ -3,15 +3,12 @@ import RPi.GPIO as GPIO # For PWM
 import time
 from gpiozero import AngularServo # For direct commands to servo
 
-servo = AngularServo(18, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-def set_angle(angle):
-    servo.angle = angle
-    time.sleep(1)
-
-try:
-    while True:
-        set_angle(10)
-
-except KeyboardInterrupt:
-    print("Program exiting...")
+while (True):
+    servo.angle = 90
+    time.sleep(2)
+    servo.angle = 0
+    time.sleep(2)
+    servo.angle = -90
+    time.sleep(2)
