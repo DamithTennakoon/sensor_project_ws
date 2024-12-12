2c4b99a04f0a10af698c72eb47fab2ce7ff1fbc0
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-12-12 14:25:39 -0500

    Updating the target servo angle over UDP server

--- src/temp_code/servo_motor.py
+++ src/temp_code/servo_motor.py
@@ -19,9 +19,24 @@ import socket
 
 # Function: decode the received msg into rpi value and move servo
 # Q = +1 deg, W = -1 deg,...
-def move_servo(msg):
-    pass
-    
+# Notes: Ensure that the operating range of the drum is between -90 (above ground - horizontal) and 0 (directly in the ground - downwards).
+def move_servo(msg, incr):
+    # Increment servo target value based on input keys
+    if (msg == 'q'):
+        incr += 1
+    elif (msg == 'w'):
+        incr -= 1
+    else:
+        pass
+    # Clamp the target servo value between -90 and -40 deg.
+    if (incr < -90):
+        incr = 90
+    elif (incr > -40):
+        incr = -40
+    else:
+        incr = incr
+
+    return incr
 
 # Construct main method
 def main():
@@ -49,7 +64,9 @@ def main():
         # Data reception and decoding
         msg_rx, client_ip = udp_server.recvfrom(buffer_size)
         msg_rx = msg_rx.decode('utf-8')
-        print("RX: ", msg_rx)
+        
+        # Compute and actuate servo 
+        print(f"TARGET ANGLE: {move_servo(msg_rx, target_servo_angle)}")
 
         # Data encoding and transmission
         udp_server.sendto(msg_tx.encode('utf-8'), client_ip)
